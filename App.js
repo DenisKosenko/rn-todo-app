deb3f71bb4e8fda5455505114d48109158b7cef2
Denis Kosenko <deniskosenko@Deniss-MacBook-Air.local>
2022-01-31 13:35:48 +0200

    RTA-1: Add login component

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginPage from '/Users/deniskosenko/Documents/rn-todo-app/loginPage';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app Ivan!</Text>
       <StatusBar style="auto" />
+      <LoginPage></LoginPage>
     </View>
   );
 }
